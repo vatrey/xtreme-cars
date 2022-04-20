fe8e83c2d1ad1285bf1eb70925f5885bd7df817c
Atrey <vatrey@deloitte.com>
2022-04-20 15:51:19 +0530

    added react routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
+import { Box } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
 import './App.scss';
+import Booking from './components/booking/Booking';
+import BookingConfirmation from './components/bookingConfirmation/BookingConfirmation';
+import CarListing from './components/carListing/CarListing';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Routes>
+        <Route path="/booking" element={
+          <Box>
+            <Box style={{ height: "5rem" }}></Box>
+            <Booking />
+          </Box>
+        } />
+        <Route path="/booking-confirmation" element={
+          <Box>
+            <Box style={{ height: "5rem" }}></Box>
+            <BookingConfirmation />
+          </Box>
+        } />
+        <Route path="/cars" element={
+          <Box>
+            <Box style={{ height: "5rem" }}></Box>
+            <CarListing />
+          </Box>
+        } />
+      </Routes>
+      <Footer />
     </div>
   );
 }
