bb7f1e92c554ced0a288784b446ca1502a57249b
Atrey <vatrey@deloitte.com>
2022-04-21 16:34:03 +0530

    imported image for the booking confirmation page

--- src/components/bookingConfirmation/BookingConfirmation.tsx
+++ src/components/bookingConfirmation/BookingConfirmation.tsx
@@ -1,28 +1,32 @@
 import { Box, Grid } from "@mui/material";
 import "./BookingConfirmation.scss";
 import Bmw from "../../assets/BMW - Explore.svg";
+import Bmwx5 from "../../assets/dummy.svg";
 
 function BookingConfirmation(): JSX.Element {
     return (
-        <Box className="main-body">
-            <Box className="white-back">
-                <Box className="sub-main-body">
-                    <Box id="merge">
-                        <Box className="success-box">
-                            {/* <img src={Bmwx5} alt="" /> */}
-                            <p>Booking <span id="heavy">Successfull</span></p>
-                            <p>
-                                <span id="download-link">
-                                    Download
-                                </span>
-                                <span id="heavy">
-                                    &nbsp; the booking summary
-                                </span>
-                            </p>
-                        </Box>
-                        <Box id="parent-overlay">
-                            <img id="bmw" src={Bmw} alt="" />
-                            <Box id="bkg" />
+        <Box>
+            <Box style={{ height: "5rem" }}></Box>
+            <Box className="main-body">
+                <Box className="white-back">
+                    <Box className="sub-main-body">
+                        <Box id="merge">
+                            <Box className="success-box">
+                                <img src={Bmwx5} alt="" />
+                                <p>Booking <span id="heavy">Successfull</span></p>
+                                <p>
+                                    <span id="download-link">
+                                        Download
+                                    </span>
+                                    <span id="heavy">
+                                        &nbsp; the booking summary
+                                    </span>
+                                </p>
+                            </Box>
+                            <Box id="parent-overlay">
+                                <img id="bmw" src={Bmw} alt="" />
+                                <Box id="bkg" />
+                            </Box>
                         </Box>
                     </Box>
                 </Box>
